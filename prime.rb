76b6cb8cddd7fcf572400679f9a2704ca1fc56fb
Dan <DanielMooreRealPerson@gmail.com>
2020-11-21 19:14:36 -0500

    commit tree 0c34fdf7fa6f6900953005be409bbace02c3b8cc

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+#def prime?(integer_arg)
+ #   if integer_arg < 2
+  #      false
+   #     (2..integer_arg/2).none?{|i| integer_arg % i == 0}
+
+#end
+
+
+    #take in the integer_arg
+    #divide it by every number from itself to 2
+    #if there 
+    def prime?(n)
+        return false if n < 2
+      
+        (2..n/2).none?{|i| n % i == 0}
+      end
