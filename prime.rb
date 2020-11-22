f4e006ab295bace15e16afee882ff53789271411
Dan <DanielMooreRealPerson@gmail.com>
2020-11-21 19:22:12 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+
+
+    #take in the integer_arg
+    #divide it by every number from itself to 2
+    #if there 
+    def prime?(n)
+        return false if n < 2
+      
+        (2..n/2).none?{|i| n % i == 0}
+      end
